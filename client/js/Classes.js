16544b22073a50437efc1f069a7008bf6d592a12
Matteo Santannera <m.santannera@campus.unimib.it>
2020-01-21 21:19:19 +0100

    creazione pedine

--- client/js/Classes.js
+++ client/js/Classes.js
@@ -1,13 +1,15 @@
 class Player {
-    constructor(id, name) {
+    constructor(id, name, pedina) {
         this.id = id;
         this.name = name;
         this.money = 1500;
         this.pos = 0;
         this.props = [];
+        this.pedina = pedina;
     }
 
     dicePos(diceNumber) {
+      let sum;
         sum = this.pos + diceNumber;
         if (sum > 39)
             return sum - 40;
@@ -29,5 +31,22 @@ class Casella {
     this.x = x;
     this.y = y;
     this.type = type;
+  }
+}
+class Pedina {
+  constructor(color) {
+    this.color = color;
+    this.w = 0;
+    this.h = 0;
+  }
+  setDimensions(w,h) {
+    this.w = w;
+    this.h = h;
+  }
+}
+class Coordinates {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
   }
 }
